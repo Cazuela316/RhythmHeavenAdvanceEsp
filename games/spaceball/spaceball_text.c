c5702fa1f612fb164b30bc9ec6bce46fe0af58bf
Luciano Molina <lucianofranciscopsoto@gmail.com>
2026-01-21 20:09:44 -0300

    Spaceball traducido!! (las calificaciones)

--- games/spaceball/spaceball_text.c
+++ games/spaceball/spaceball_text.c
@@ -5,70 +5,70 @@
 /* Game Text - Spaceball */
 
 
-const char D_0805bdc8[] = "You needed to focus on the basics.";
+const char D_0805bdc8[] = "Tienes que enfocarte en lo basico.";
 
 const char D_0805bde4[] = "";
 
-const char D_0805bde8[] = "Your grip was weak.";
+const char D_0805bde8[] = "Tu agarre fue debil.";
 
-const char D_0805be00[] = "Nice grip!";
+const char D_0805be00[] = "Tienes un buen agarre!";
 
-const char D_0805be1c[] = "You relied too much on the screen.";
+const char D_0805be1c[] = "Te enfocaste mucho en la pantalla.";
 
-const char D_0805be34[] = "You could bat while blindfolded!";
+const char D_0805be34[] = "Podrias hasta batear a ciegas!";
 
-const char D_0805be50[] = "The speed tripped you up.";
+const char D_0805be50[] = "La velocidad te gano.";
 
-const char D_0805be6c[] = "The speed was no match for you!";
+const char D_0805be6c[] = "Lo hiciste muy bien en las partes rapidas!";
 
-const char D_0805be80[] = "You needed to focus on the basics.";
+const char D_0805be80[] = "Tienes que enfocarte en lo basico.";
 
 const char D_0805be9c[] = "";
 
-const char D_0805bea0[] = "Your grip was weak.";
+const char D_0805bea0[] = "Tu agarre fue debil.";
 
-const char D_0805beb8[] = "Nice grip!";
+const char D_0805beb8[] = "Tienes un buen agarre!";
 
-const char D_0805bed4[] = "You relied too much on the screen.";
+const char D_0805bed4[] = "Te enfocaste mucho en la pantalla.";
 
-const char D_0805beec[] = "You could bat while blindfolded!";
+const char D_0805beec[] = "Podrias hasta batear a ciegas!";
 
-const char D_0805bf08[] = "The speed tripped you up.";
+const char D_0805bf08[] = "La velocidad te gano.";
 
-const char D_0805bf24[] = "The speed was no match for you!";
+const char D_0805bf24[] = "Lo hiciste muy bien en las partes rapidas!";
 
-const char D_0805bf38[] = "You needed to focus on the basics.";
+const char D_0805bf38[] = "Tienes que enfocarte en lo basico.";
 
 const char D_0805bf54[] = "";
 
-const char D_0805bf58[] = "Your grip was weak.";
+const char D_0805bf58[] = "Tu agarre fue debil.";
 
-const char D_0805bf70[] = "Nice grip!";
+const char D_0805bf70[] = "Tienes un buen agarre!";
 
-const char D_0805bf8c[] = "You couldn't cope when you couldn't see.";
+const char D_0805bf8c[] = "No te las podrias arreglar sin ver la pantalla.";
 
-const char D_0805bfa8[] = "You could bat while blindfolded!";
+const char D_0805bfa8[] = "Podrias hasta batear a ciegas!";
 
-const char D_0805bfc4[] = "The speed tripped you up.";
+const char D_0805bfc4[] = "La velocidad te gano.";
 
-const char D_0805bfe0[] = "You did well during the fast part!";
+const char D_0805bfe0[] = "Lo hiciste muy bien en las partes rapidas!";
 
-const char D_0805bff8[] = "Scorecard";
+const char D_0805bff8[] = "Tarjeta de puntuaci‡in";
 
-const char D_0805c004[] = "You needed to focus on the basics.";
+const char D_0805c004[] = "Tienes que enfocarte en lo basico.";
 
 const char D_0805c020[] = "";
 
-const char D_0805c024[] = "Your grip was weak.";
+const char D_0805c024[] = "Tu agarre fue debil.";
 
-const char D_0805c03c[] = "Nice grip!";
+const char D_0805c03c[] = "Tienes un buen agarre!";
 
-const char D_0805c058[] = "You couldn't cope when you couldn't see.";
+const char D_0805c058[] = "No te las podrias arreglar sin ver la pantalla.";
 
-const char D_0805c074[] = "You could bat while blindfolded!";
+const char D_0805c074[] = "Podrias hasta batear a ciegas!";
 
-const char D_0805c090[] = "The speed tripped you up.";
+const char D_0805c090[] = "La velocidad te gano.";
 
-const char D_0805c0ac[] = "You did well during the fast part!";
+const char D_0805c0ac[] = "Lo hiciste muy bien en las partes rapidas!";
 
-const char D_0805c0c4[] = "Scorecard";
+const char D_0805c0c4[] = "Tarjeta de puntuaci‡in";
