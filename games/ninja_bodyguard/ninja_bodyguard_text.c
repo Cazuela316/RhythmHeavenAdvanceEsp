575377c9b6f2e1833ac2d291d3fdd1d2cbfa7891
TheSpheric <adrianguipe@example.com>
2026-01-22 06:30:06 -0400

    ninja bodyguard

--- games/ninja_bodyguard/ninja_bodyguard_text.c
+++ games/ninja_bodyguard/ninja_bodyguard_text.c
@@ -5,32 +5,32 @@
 /* Game Text - Ninja Bodyguard */
 
 
-const char D_0805b8e0[] = "I hired you to protect me.";
+const char D_0805b8e0[] = "Te contrat‡Q para protegerme.";
 
 const char D_0805b8f8[] = "";
 
-const char D_0805b8fc[] = "Your swordplay skills were lacking.";
+const char D_0805b8fc[] = "Tus habilidades de espada son deficientes.";
 
-const char D_0805b924[] = "You're quite nimble with a sword!";
+const char D_0805b924[] = "Eres ‡cgil con la espada!";
 
-const char D_0805b944[] = "Their four-arrow barrage overwhelmed you.";
+const char D_0805b944[] = "Fuiste abrumado por sus flechas.";
 
-const char D_0805b968[] = "You blocked their four-arrow barrage!";
+const char D_0805b968[] = "Bloqueaste todas sus flechas!";
 
-const char D_0805b988[] = "My Lord solemnly intones...";
+const char D_0805b988[] = "Mi noble solemnemente entona...";
 
-const char D_0805b99c[] = "I thought you were trying to protect me...";
+const char D_0805b99c[] = "Pens‡Q que tratabas de protegerme...";
 
 const char D_0805b9b4[] = "";
 
-const char D_0805b9b8[] = "Your stick swipes weren't quick enough.";
+const char D_0805b9b8[] = "Tus golpes con la vara no son suficientemente r‡cpidos.";
 
-const char D_0805b9dc[] = "You're pretty quick with a stick!";
+const char D_0805b9dc[] = "Eres bastante r‡cpido con la vara!";
 
-const char D_0805b9fc[] = "Their five-rock barrage overwhelmed you.";
+const char D_0805b9fc[] = "Su r‡cfaga de rocas te abrum‡i.";
 
-const char D_0805ba1c[] = "You blocked their five-rock barrage!";
+const char D_0805ba1c[] = "Bloqueaste todas las rocas!";
 
-const char D_0805ba40[] = "You deflected two rocks in one go! Brilliant!";
+const char D_0805ba40[] = "Desviaste dos rocas a la vez! Brillante!";
 
-const char D_0805ba68[] = "My girlfriend calmly comments...";
+const char D_0805ba68[] = "Mi novia con calma comenta...";
