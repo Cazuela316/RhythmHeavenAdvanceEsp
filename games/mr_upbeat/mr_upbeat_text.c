994a6be63d18920309e23fd43c6ec9c59a5fc24e
Luciano Molina <lucianofranciscopsoto@gmail.com>
2026-05-21 23:10:08 -0400

    okei no me habia dado cuenta que el van 4! de karate killo extra no estaba traducido!? asi que lo traduje jiji

--- games/mr_upbeat/mr_upbeat_text.c
+++ games/mr_upbeat/mr_upbeat_text.c
@@ -5,34 +5,34 @@
 /* Game Text - Mr. Upbeat */
 
 
-const char D_08068170[] = "Salta la aguja!";
+const char D_08068170[] = "ˆ¿Salta la aguja!";
 
 const char D_0806817c[] = "Eso es.";
 
-const char D_08068188[] = "Muy bien!";
+const char D_08068188[] = "ˆ¿Muy bien!";
 
-const char D_08068194[] = "Excelente!";
+const char D_08068194[] = "ˆ¿Excelente!";
 
-const char D_080681a0[] = "No seras el elegido?";
+const char D_080681a0[] = "ˆ¾No seras el elegido?";
 
-const char D_080681ac[] = "Que habilidad, mis pies se durmieron!";
+const char D_080681ac[] = "Que habilidad, ˆ¿mis pies se durmieron!";
 
-const char D_080681bc[] = "Caramba!";
+const char D_080681bc[] = "ˆ¿Caramba!";
 
-const char D_080681c8[] = "Imposible que seas asi de bueno!";
+const char D_080681c8[] = "ˆ¿Imposible que seas asi de bueno!";
 
-const char D_080681d8[] = "A que estas haciendo trampa, no?";
+const char D_080681d8[] = "A que estas haciendo trampa, ˆ¾no?";
 
-const char D_080681e8[] = "Acaso mordiste algo radiactivo?";
+const char D_080681e8[] = "ˆ¾Acaso mordiste algo radiactivo?";
 
-const char D_080681f0[] = "COMO ESTAS LOGRANDO ESTO!?";
+const char D_080681f0[] = "ˆ¾ˆ¿COMO ESTAS LOGRANDO ESTO!?";
 
-const char D_080681fc[] = "Nooo! ME DUELE!";
+const char D_080681fc[] = "ˆ¾ˆ¿Nooo! ˆ¿ME DUELE!";
 
-const char D_0806820c[] = "Cuando podre parar?";
+const char D_0806820c[] = "ˆ¾Cuando podre parar?";
 
 const char D_0806821c[] = "...Ayuda...me....";
 
 const char D_0806822c[] = "Un descanso...por favor...";
 
-const char D_08068238[] = "Que cansancio... Estoy alucinando...?";
+const char D_08068238[] = "Que cansancio... ˆ¾Estoy alucinando...?";
